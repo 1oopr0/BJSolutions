c2f128021f0e9ab0429a62e8c5e99217d863b259
승민 백 <bsm7307@gmail.com>
2020-06-29 04:17:03 +0900

    초기 커밋.

---
+++ BJSolutions/Program.cs
@@ -0,0 +1,17 @@
+﻿using BJSolutions.Solutions;
+
+namespace BJSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solve(new P17931());
+        }
+
+        static void Solve(IProblem problem)
+        {
+            problem.Solve();
+        }
+    }
+}
---
+++ BJSolutions/Solutions/P17931.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BJSolutions.Solutions
+{
+    class P17931 : IProblem
+    {
+        public void Solve()
+        {
+            var n = Console.ReadLine();
+            var seq = Console.ReadLine();
+
+            var length = int.Parse(n);
+            var seqs = seq.Split();
+
+            StringBuilder answer = new StringBuilder();
+            int count = 1;
+            int prev = int.Parse(seqs[0]);
+            answer.Append(seqs[0] + " ");
+            for (int i = 1; i < length; i++)
+            {
+                var num = int.Parse(seqs[i]);
+                if (prev < num)
+                {
+                    answer.Append(seqs[i]);
+                    answer.Append(" ");
+                    count++;
+                    prev = num;
+                }
+            }
+
+            Console.WriteLine(count);
+            Console.WriteLine(answer.ToString());
+        }
+    }
+}
